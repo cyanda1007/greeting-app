624d210266f9a11a4bb25a357f7eec904be71ed1
cyanda1007 <siyaphiwa196@gmail.com>
2022-07-15 12:32:35 +0200

    added modified files

--- greeting.dom.js
+++ greeting.dom.js
@@ -2,23 +2,34 @@ let domFunction = Greetings();
 const disLanguage = document.querySelector(".typeOfLanguage");
 const greetBtn = document.querySelector(".addLanguage");
 const firstName = document.querySelector(".languageEntered");
-const radioBtn = document.querySelector("input[name='language']");
+const radioZulu = document.querySelector(".radioZulu");
+const radioEnglish = document.querySelector(".radioEnglish");
+const radioXhosa = document.querySelector(".radioXhosa");
 const clearBtn = document.querySelector(".remove");
 const calculate = document.querySelector(".count");
 const errorMessage = document.querySelector(".name-error");
 const languageErrorMessage = document.querySelector(".language-error");
-
+calculate.innerHTML =
+  localStorage.getItem("list") === null
+    ? 0
+    : JSON.parse(localStorage.getItem("list")).length;
 //get all items in local storage
 let listName = [];
+
 // check if buttons clicked
 let checked = false;
-radioBtn.addEventListener("click", () => {
+radioZulu.addEventListener("click", () => {
+  checked = true;
+});
+radioEnglish.addEventListener("click", () => {
+  checked = true;
+});
+radioXhosa.addEventListener("click", () => {
   checked = true;
 });
 
 greetBtn.addEventListener("click", (e) => {
   e.preventDefault();
-
   let name = firstName.value;
 
   const checkedRadioBtn = document.querySelector(
@@ -26,7 +37,7 @@ greetBtn.addEventListener("click", (e) => {
   );
 
   if (name === "" || name === undefined) {
-    errorMessage.innerHTML = "Please enter a name";
+    errorMessage.style.display = "block";
     throw new Error("Please enter a name");
   } else if (/[0-9]/.test(name)) {
     errorMessage.innerHTML = "Please put letters only";
@@ -40,22 +51,30 @@ greetBtn.addEventListener("click", (e) => {
 
   errorMessage.style.display = "none";
   languageErrorMessage.style.display = "none";
-  calculate.innerHTML = domFunction.counter();
-
-  localStorage.setItem("number", JSON.stringify(count));
 
   if (checkedRadioBtn.value === "zulu") {
     disLanguage.innerHTML = domFunction.zuluGreeting(name);
-  } else if (checkedRadioBtn.value === "engfa") {
+  }
+  if (checkedRadioBtn.value === "engfa") {
+    console.log(domFunction.englishGreeting(name));
     disLanguage.innerHTML = domFunction.englishGreeting(name);
-  } else if (checkedRadioBtn.value === "xhosa") {
+  }
+  if (checkedRadioBtn.value === "xhosa") {
     disLanguage.innerHTML = domFunction.xhosaGreeting(name);
   }
   listName.push(name);
   localStorage.setItem("list", JSON.stringify(listName));
-  console.log(listName);
+  localStorage.getItem("list", JSON.stringify(listName));
+  calculate.innerHTML =
+    localStorage.getItem("list") === null
+      ? 0
+      : JSON.parse(localStorage.getItem("list")).length;
 });
-// clearBtn.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   localStorage.clear(JSON.stringify(name), JSON.stringify(name));
-// });
+clearBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  localStorage.clear("list", JSON.stringify(listName));
+  calculate.innerHTML =
+    localStorage.getItem("list") === null
+      ? 0
+      : JSON.parse(localStorage.getItem("list")).length;
+});
